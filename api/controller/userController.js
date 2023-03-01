60e13a50c7318afa5297fc2e927102d145b0f97d
Tapendra18 <107476177+Tapendra18@users.noreply.github.com>
2023-03-01 17:07:54 +0530

    reset password done mail send

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -2,46 +2,10 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs")
 const SECRET_KEY = "NODEAPI"
 const users = require("../models/userModel");
+// const utility = require("../utility/utils");
 const nodemailer = require('nodemailer');
-const randomstring = require("randomstring");
 // const liveController = require('./collegeController');
 
-const sendresetpasswordMail = async (name, email, token) => {
-    try {
-
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 4000,
-            secure: false,
-            requireTLS: true,
-            auth: {
-                user: "<tapendrapratapsingh2@gmail.com>",
-                pass: "tapendra@12"
-            }
-        });
-
-        const mailOptions = {
-            from: "<tapendrapratapsingh2@gmail.com>",
-            to: "<noreply12@gmail.com>",
-            subject: 'for reset password ',
-            html: '<p> hii ' + name + ', Please copy the link<a href="http://192.168.1.15:4000/api/v1/contact/add?token=' + token + '"> reset your password </a>'
-        }
-
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error)
-            } else {
-                console.log("Mail has been send :-", info.response)
-            }
-        })
-
-    } catch (err) {
-        res.status(400).send({
-            success: false,
-            msg: err
-        })
-    }
-}
 
 exports.userRegister = async (req, res) => {
     const { fname, email, password } = req.body;
@@ -97,25 +61,115 @@ exports.userlogin = async (req, res) => {
     }
 }
 
+// exports.forgetpassword = async (req, res) => {
+//     try {
+//         const email = req.body.email
+//         const userData = await users.findOne({ email: email });
+//         if (userData) {
+//             const RandomString = randomstring.generate();
+//             const data = await users.updateOne({ email: email }, { $set: { token: RandomString } });
+//             console.log(data, "dataaaa");
+//             sendresetpasswordMail(userData.name, userData.email, RandomString);
+//             res.status(200).send({ success: true, msg: "Please check your inbox of mail reset your password" })
+
+//         } else {
+//             res.status(400).send({ success: true, msg: "this email does not exits" })
+//         }
+
+//     } catch (err) {
+//         res.status(400).send({
+//             success: false,
+//             msg: err
+//         })
+//     }
+// }
+
 exports.forgetpassword = async (req, res) => {
     try {
-        const email = req.body.email
-        const userData = await users.findOne({ email: email });
-        if (userData) {
-            const RandomString = randomstring.generate();
-            const data = await users.updateOne({ email: email }, { $set: { token: RandomString } });
-            console.log(data , "dataaaa");
-            sendresetpasswordMail(userData.name ,userData.email , RandomString);
-            res.status(200).send({ success: true, msg: "Please check your inbox of mail reset your password" })
-
-        } else {
-            res.status(400).send({ success: true, msg: "this email does not exits" })
-        }
-
-    } catch (err) {
-        res.status(400).send({
-            success: false,
-            msg: err
-        })
+       const { email } = req.body;
+       console.log(req.body);
+ 
+       const existingUser = await users.findOne({ email: req.body.email });
+       console.log(existingUser)
+       if (!existingUser) {
+          return res.status(404).json({ message: 'User not found' });
+       }
+ 
+       const secret = SECRET_KEY + existingUser.password;
+       const payload = ({
+          email: existingUser.email,
+          id: existingUser.id
+       });
+       const newtoken = jwt.sign(payload, secret, { expiresIn: '15m' });
+       const link = `http://192.168.1.15:4000/api/v1/user/resetpassword`
+       console.log(link);
+ 
+       const transporter = nodemailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 587,
+          auth: {
+             user: 'pythondevforsure@gmail.com',
+             pass: 'idbbbcuygqhwqedx'
+          }
+       });
+       const mailOptions = {
+          to: req.body.email,
+          name: existingUser.name,
+          subject: 'password reset link',
+          text: `Click here to reset Password :` + link,
+       };
+ 
+       transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+             console.error('email not sent', error);
+          }
+          else {
+             console.log('email sent', info.response);
+          }
+       });
+       return res.status(200).json({
+          message: 'password reset link sent successfully',
+          email: req.body.email
+       });
+ 
+    } catch (error) {
+       console.log(error);
+       res.status(500).json({
+          message: 'Somthing went wrong',
+          email: 'req.body.email'
+       });
     }
-}
+ };
+
+ exports.updatepass = async (req, res) => {
+    try {
+       console.log(req.body);
+       const existingUser = await users.findOne({ token: req.body.token });
+       console.log(existingUser)
+       if (!existingUser) {
+          return res.status(404).json({ message: 'User not found' });
+       }
+       const matchPassword = await bcrypt.compare(req.body.oldpassword, existingUser.password);
+ 
+       if (!matchPassword) {
+          return res.status(400).json({ message: 'Old Password is not matched' });
+       }
+       const salt = await bcrypt.genSalt(10);
+       const hashedupdatedpass = await bcrypt.hash(req.body.newpassword, salt)
+       console.log(hashedupdatedpass, 'passssssssssssssssssss')
+ 
+       const newtoken = await jwt.sign({ email: existingUser.email }, SECRET_KEY);
+       existingUser.update({ $set: { password: hashedupdatedpass, token: newtoken } }, (error, result) => {
+          if (error) {
+             console.log(error);
+          }
+       });
+       return res.status(201).json({ existingUser, token: newtoken });
+ 
+    } catch (error) {
+       console.log(error);
+       res.status(500).json({
+          message: 'Somthing went wrong',
+       });
+    }
+ }
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -5,7 +5,7 @@ const userController =  require("../controller/userController");
 router.post("/signup" , userController.userRegister);
 router.post("/login", userController.userlogin);
 router.post('/resetpassword' , userController.forgetpassword);
-
+router.post('/updatepassword' , userController.updatepass)
 
 
 module.exports = router
